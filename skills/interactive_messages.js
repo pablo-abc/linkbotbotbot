2052d4c3d562599bacf47d2f9428b2e95bd22fb3
Glitch (gigantic-guan) <none>
2018-07-18 15:53:53 +0000

    ⚓️📺 Checkpoint ./skills/interactive_messages.js:1466919/42

--- skills/interactive_messages.js
+++ skills/interactive_messages.js
@@ -5,7 +5,6 @@ module.exports = function(controller) {
     controller.middleware.receive.use(function(bot, message, next) {
       if (message.type == 'interactive_message_callback') {
         if (message.actions[0].name.match(/^delete$/)){
-          console.log(message.actions[0])
           controller.storage.links.get(message.actions[0].value, (err, resp) => {
             console.log(err)
             bot.reply(message, 'The link has been deleted')
