a078b1a4a7fd5717e1a5a5454d261f395548d1a0
Glitch (gigantic-guan) <none>
2018-07-18 19:49:09 +0000

    👶🐴 Checkpoint ./skills/events.js:1466919/567

--- skills/events.js
+++ skills/events.js
@@ -8,6 +8,13 @@ module.exports = function(controller) {
     
     controller.on('reaction_added', (bot, event) => {
       console.log(event)
-      
+      if (!event.reaction.includes('+1') && !event.reaction.includes('thumbsup'))
+        return
+      controller.storage.links.find({ts: event.item.ts, channel: event.item.channel})
+      .then(links => {
+          if (links.length === 0)
+            return
+          bot.api.reactions.get({ts: links[0].ts, channel: links[0].channelId})
+        })
     });
 }
