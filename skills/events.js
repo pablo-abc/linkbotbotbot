d49ed336000c7deb6d393849b1c32c1568ad0d1a
Pablo Berganza <pberganza10@gmail.com>
2018-07-18 15:53:22 -0600

    Standard and refactor

--- skills/events.js
+++ skills/events.js
@@ -1,30 +1,26 @@
-module.exports = function(controller) {
+module.exports = function (controller) {
+  controller.on('user_channel_join,user_group_join', function (bot, message) {
+    bot.reply(message, 'Welcome, <@' + message.user + '>')
+  })
 
-    controller.on('user_channel_join,user_group_join', function(bot, message) {
-
-        bot.reply(message, 'Welcome, <@' + message.user + '>');
-
-    });
-    
-    controller.on('reaction_added,reaction_removed', (bot, event) => {
-      if (!event.reaction.includes('+1') && !event.reaction.includes('thumbsup'))
-        return
-      controller.storage.links.find({ts: event.item.ts, channelId: event.item.channel})
+  controller.on('reaction_added,reaction_removed', (bot, event) => {
+    if (!event.reaction.includes('+1') && !event.reaction.includes('thumbsup')) { return }
+    controller.storage.links.find({ts: event.item.ts, channel: event.item.channel})
       .then(links => {
-          if (links.length === 0)
-            return
-          links.map(link => {
-            bot.api.reactions.get({timestamp: links[0].ts, channel: links[0].channelId}, (err, response) => {
-              if (!response.message.reactions) response.message.reactions = []
-              const tCount = response.message.reactions.reduce((total, reaction) => {
-                return reaction.name.includes('+1') || reaction.name.includes('thumbsup') ? 
-                  total + reaction.count : 
-                  total
-              }, 0)
-              link.thumbsup = tCount
-              controller.storage.links.save(link)
-            })
+        if (links.length === 0) { return }
+        links.map(link => {
+          bot.api.reactions.get({timestamp: links[0].ts, channel: links[0].channel}, (err, response) => {
+            if (err) return console.log(err)
+            if (!response.message.reactions) response.message.reactions = []
+            const tCount = response.message.reactions.reduce((total, reaction) => {
+              return reaction.name.includes('+1') || reaction.name.includes('thumbsup')
+                ? total + reaction.count
+                : total
+            }, 0)
+            link.thumbsup = tCount
+            controller.storage.links.save(link)
           })
         })
-    });
+      })
+  })
 }
--- skills/link-hear.js
+++ skills/link-hear.js
@@ -1,14 +1,13 @@
 module.exports = controller => {
-  const parseLinks = (bot, message, limit, links, answer)=> {
-            const parsedLinks = links.reduce((result, link) => {
-              if (limit && link.createdAt < limit)
-                return result
-              let tags = link.tags ? link.tags : 'No tags for this link'
-              tags = tags.toString().replace(/,/g, ', ')
-              const line = `┌ Link ${link.link}. Added${answer}\n >─── Tags: \`${tags}\`. Date: _${new Date(link.createdAt * 1000)}_\n└─── ${link.thumbsup}:+1:`
-              return `${result}${line}\n`
-            }, ``)
-            bot.reply(message, parsedLinks)
+  const parseLinks = (bot, message, limit, links, answer) => {
+    const parsedLinks = links.reduce((result, link) => {
+      if (limit && link.createdAt < limit) return result
+      let tags = link.tags ? link.tags : 'No tags for this link'
+      tags = tags.toString().replace(/,/g, ', ')
+      const line = `┌ Link ${link.link}. Added${answer}\n >─── Tags: \`${tags}\`. Date: _${new Date(link.createdAt * 1000)}_\n└─── ${link.thumbsup}:+1:`
+      return `${result}${line}\n`
+    }, ``)
+    bot.reply(message, parsedLinks)
   }
 
   controller.hears(
@@ -16,38 +15,37 @@ module.exports = controller => {
     'direct_message,mention,direct_mention',
     (bot, message) => {
       const mReg = /https?:\/\/((www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*))/.exec(message.match[1])
-      controller.storage.links.find({userId: message.user, link: '<' + mReg[0].toLowerCase() + '>', channelId: message.channel})
-      .then(links => {
-        if (links.length === 0)
-          return bot.reply(message, 'You have not stored this link here')
-        bot.reply(message, {
-          attachments: [
-            {
-              title: 'Do you want to delete this link?',
-              text: message.match[1],
-              callback_id: `delete_link`,
-              attachment_type: 'default',
-              actions: [
-                {
-                  name: 'delete',
-                  text: 'Delete',
-                  value: message.channel + message.user + mReg[0],
-                  type: 'button',
-                  style: 'danger',
-                  confirm: {
-                    title: 'Are you sure?',
-                    text: 'This will delete this link permanently from this channel',
-                    ok_text: 'I am sure',
-                    dismiss_text: 'Not sure, actually'
+      controller.storage.links.find({user: message.user, link: '<' + mReg[0].toLowerCase() + '>', channel: message.channel})
+        .then(links => {
+          if (links.length === 0) return bot.reply(message, 'You have not stored this link here')
+          bot.reply(message, {
+            attachments: [
+              {
+                title: 'Do you want to delete this link?',
+                text: message.match[1],
+                callback_id: `delete_link`,
+                attachment_type: 'default',
+                actions: [
+                  {
+                    name: 'delete',
+                    text: 'Delete',
+                    value: message.channel + message.user + mReg[0],
+                    type: 'button',
+                    style: 'danger',
+                    confirm: {
+                      title: 'Are you sure?',
+                      text: 'This will delete this link permanently from this channel',
+                      ok_text: 'I am sure',
+                      dismiss_text: 'Not sure, actually'
+                    }
                   }
-                }
                 ]
-            }
+              }
             ]
+          })
         })
-      })
     })
-  
+
   controller.hears(
     /(<https?:\/\/((www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*))(\|\2)?>( *\[[a-z][a-z0-9]*\])*)/ig,
     'direct_message,mention,direct_mention,ambient',
@@ -58,14 +56,14 @@ module.exports = controller => {
         const mReg = /https?:\/\/((www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_+.~#?&//=]*))/.exec(m)
         const id = message.channel + message.user + mReg[0]
         const link = '<' + mReg[0].toLowerCase() + '>'
-        const userId = message.user
-        const channelId = message.channel
+        const user = message.user
+        const channel = message.channel
         const ts = message.ts
         const team = message.team
         const createdAt = message.event_time
         const linkInfo = {
-          userId,
-          channelId,
+          user,
+          channel,
           id,
           link,
           ts,
@@ -83,44 +81,42 @@ module.exports = controller => {
           })
       }
     })
-  
+
   controller.hears(
-    /links shared(( by (<@([^>]*)>|me))?( (on|in) ((this channel)|(<#([^\|>]*)\|.*>)))?)?( (from )?this (week|month|day))?( about (([a-z][a-z0-9]*(, [a-z][a-z0-9]*)*)( (and|or) ([a-z][a-z0-9]*))?))?/i,
+    /links shared(( by (<@([^>]*)>|me))?( (on|in) ((this channel)|(<#([^|>]*)\|.*>)))?)?( (from )?this (week|month|day))?( about (([a-z][a-z0-9]*(, [a-z][a-z0-9]*)*)( (and|or) ([a-z][a-z0-9]*))?))?/i,
     'direct_message,direct_mention,mention',
     (bot, message) => {
       let options = {}
       let answer = ``
       let limit = null
       let tags = []
-      let channel = message.channel
       if (message.match[3]) {
         if (message.match[4]) {
-          options.userId = message.match[4]
+          options.user = message.match[4]
           answer += ` by <@${message.match[4]}>`
         } else {
-          options.userId = message.user
+          options.user = message.user
           answer += ` by you`
         }
       }
       if (message.match[10]) {
-        options.channelId = message.match[10]
-        channel = message.match[10]
+        options.channel = message.match[10]
         answer += ` on <#${message.match[10]}>`
       }
       if (message.match[7] || !message.match[7]) {
-        options.channelId = message.channel
+        options.channel = message.channel
         answer += ` on this channel`
       }
       if (message.match[13]) {
         switch (message.match[13].toLowerCase()) {
           case 'day':
-            limit = new Date().getTime()/1000 - 86400
+            limit = new Date().getTime() / 1000 - 86400
             break
           case 'week':
-            limit = new Date().getTime()/1000 - 604800
+            limit = new Date().getTime() / 1000 - 604800
             break
           case 'month':
-            limit = new Date().getTime()/1000 - 2592000
+            limit = new Date().getTime() / 1000 - 2592000
             break
         }
       }
@@ -155,14 +151,14 @@ module.exports = controller => {
       let options = {}
       let answer = ``
       if (message.match[3]) {
-        options = {userId: message.match[3]}
+        options = {user: message.match[3]}
         answer = ` by <@${message.match[3]}>`
       }
       if (message.match[6]) {
-        options = {channelId: message.match[6]}
+        options = {channel: message.match[6]}
         answer = ` in <#${message.match[6]}>`
       } else if (message.match[4]) {
-        options = {channelId: message.channel}
+        options = {channel: message.channel}
         answer = ` in this channel`
       }
       controller.storage.links.find(options)
