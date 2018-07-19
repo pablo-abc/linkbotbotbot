7e9f674b4e19d6d9dbc960ac3d6d66cb61c9ebd1
Glitch (gigantic-guan) <none>
2018-07-19 23:15:39 +0000

    🙌🍿 Checkpoint ./package.json:1466919/2025 ./skills/register-user.js:1466919/456

--- skills/register-user.js
+++ skills/register-user.js
@@ -1,7 +1,12 @@
-const request = require('request')
+const axios = require('axios')
+const apiUrl = process.env.LINK_API_URL
 
 module.exports = controller => {
   controller.hears(/register "(.*)"/i, 'direct_message', (bot, message) => {
+    bot.api.users.info({user: message.user}, (err, response) => {
+      console.log(response)
+    })
+    // axios.post(apiUrl + '/users', params)
     bot.reply(message, `You asked me to register ${message.match[1]}`)
   })
 }
