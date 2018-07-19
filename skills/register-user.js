ea6fad73284245e1a3a9bf614578ffbbc0f9b9ff
Glitch (gigantic-guan) <none>
2018-07-19 21:09:05 +0000

    💾🚩 Checkpoint ./skills/register-user.js:1466919/62

--- skills/register-user.js
+++ skills/register-user.js
@@ -1,5 +1,7 @@
+const request = require('request')
+
 module.exports = controller => {
-  controller.hears(/register /i, 'direct_message', (bot, message) => {
-    bot.reply(message, `You asked me to register ${message.match[0]}`)
+  controller.hears(/register "(.*)"/i, 'direct_message', (bot, message) => {
+    bot.reply(message, `You asked me to register ${message.match[1]}`)
   })
 }
