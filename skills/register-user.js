570ba054f02006b75e882a6e80216a8f3a286d8d
Glitch (gigantic-guan) <none>
2018-07-20 00:29:02 +0000

    🏩🚧 Checkpoint ./skills/register-user.js:1466919/168

--- skills/register-user.js
+++ skills/register-user.js
@@ -7,7 +7,9 @@ module.exports = controller => {
       const params = {
         username: response.user.name,
         password: message.match[1],
-        email: response.user.profile.email
+        email: response.user.profile.email,
+        slackId: message.user,
+        teamId: response.user.team_id
       };
       axios.post(apiUrl + '/users', params)
       .then(response => {
@@ -15,11 +17,12 @@ module.exports = controller => {
           bot.reply(message, 'User created')
         else bot.reply(message, 'There was a problem')
       }).catch(err => {
+        console.log(err)
         try {
           const error = err.response.data.error
           if (error.details.messages.email || error.details.messages.username)
             bot.reply(message, 'User or email already exist')
-        } catch(err) {
+        } catch(erra) {
           bot.reply(message, 'There was a problem')
         }
       })
